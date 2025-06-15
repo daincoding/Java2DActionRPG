db7e66247605587721b999eb90830f07e16197b2
daincoding <official.toyou@gmail.com>
2025-06-15 10:52:37 +0200

    Added Player Sprite and directional movement

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,10 +1,17 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     // -- THIS WILL BE THE PARENT CLASS FOR ALL CHARACTERS
 
     public int x, y;
     public int speed;
+
+
+    // -- Buffered Image describes an Image with an accessible buffer of image data. It is like a storage.
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
 
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,11 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity {
 
@@ -20,17 +24,46 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     //endregion
+
+    //region Player Defaults &  Images
 
     public void setDefaultValues(){
 
         x = 100;
         y = 100;
         speed = 5;
+        direction = "down";
 
     }
+
+    public void getPlayerImage() {
+
+        // -- We load the pngs here in this segment to deliver it further.
+
+        try {
+
+            // -- requireNonNull Object means that it prevents the ImageIO to get something that is empty.
+
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
+
+        }catch(IOException e) {
+            System.err.println("Failed to load player images: " + e.getMessage()); // <-- Message if there is an Error.
+        }
+
+    }
+
+    //endregion
 
     //region UPDATE & DRAW METHOD
 
@@ -39,12 +72,16 @@ public class Player extends Entity {
     public void update(){
 
         if (keyH.upPressed) {
+            direction = "up";
             y -= speed; // <-- In Java the left upper Corner is 0 so going Up is decreasing Y and increasing when go down.
         } else if (keyH.downPressed) {
+            direction = "down";
             y += speed;
         } else if (keyH.leftPressed) {
+            direction = "left";
             x -= speed;
         } else if (keyH.rightPressed) {
+            direction = "right";
             x += speed; // <-- In Java X increases to the right.
         }
 
@@ -52,10 +89,30 @@ public class Player extends Entity {
 
     public void draw(Graphics2D g2){
 
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+            default:
+                image = down2;
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
 
     //endregion
+
 }
