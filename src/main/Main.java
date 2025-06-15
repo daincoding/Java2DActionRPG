572ff9385af9ecd260209c2ca63d48385871f24a
daincoding <official.toyou@gmail.com>
2025-06-15 08:32:52 +0200

    Uploaded Main

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame(); // <-- Window Object by Java
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // <-- Get closing Button
+        window.setResizable(false); // <-- You cant resize it.
+        window.setTitle("2D Action RPG");
+
+        GamePanel gamePanel = new GamePanel(); // <-- Call the object GamePanel
+        window.add(gamePanel); // <-- add it to the Window (JFrame)
+        window.pack();
+
+        window.setLocationRelativeTo(null);  // <-- Window is in the Middle of the Screen
+        window.setVisible(true); // <-- You can see it
+
+    }
+}
